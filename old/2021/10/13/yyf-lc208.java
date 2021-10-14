4e108cf893d4912808afc4135700520008ec43b1
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-10-13 22:03:46 -0700

    Create yyf-lc208.java

---
+++ old/2021/10/13/yyf-lc208.java
@@ -0,0 +1,60 @@
+class Trie {
+    private TrieNode root;
+    
+    class TrieNode {
+        char val;
+        String endWord;
+        TrieNode[] children = new TrieNode[26];
+        public TrieNode (char val) {
+            this.val = val;
+        }
+    }
+
+    public Trie() {
+        root = new TrieNode('a');
+    }
+    
+    public void insert(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                current.children[c - 'a'] = new TrieNode(c);
+            }
+            current = current.children[c - 'a'];
+        }
+        current.endWord = word;    
+    }
+    
+    public boolean search(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                return false;
+            }
+            current = current.children[c - 'a'];
+        }
+        return current.endWord != null && current.endWord.equals(word);
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                return false;
+            }
+            current = current.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
