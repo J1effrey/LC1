eb83e107048a68e6f2e74ce64b01ce0b00c197f5
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-10-16 15:19:35 -0700

    Create yyf-lc207.java

---
+++ new/2021/10/16/yyf-lc207.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null) {
+            return true;
+        }
+        
+        int[] inDegree = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] res = new int[numCourses];
+        int index = 0;
+        for (int i = 0; i < prerequisites.length; i++) {
+            int pre = prerequisites[i][1];
+            int post = prerequisites[i][0];
+            inDegree[post]++;
+            List<Integer> posts = map.getOrDefault(pre, new ArrayList<>());
+            posts.add(post);
+            map.put(pre, posts);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            res[index++] = course;
+            if (map.containsKey(course)) {
+                for (Integer n : map.get(course)) {
+                    if(--inDegree[n] == 0) {
+                        queue.offer(n);
+                    }
+                }
+            }
+        }
+        
+        return index == numCourses ? true : false;
+    }
+}
