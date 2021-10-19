5becd475e5b08e79231b8c390e2bbaab90f0a2e9
wfyin <wf.yin12@gmail.com>
2021-10-18 21:10:44 -0700

    Create wf-lc34.java

---
+++ new/2021/10/18/wf-lc34.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int[] res = new int[]{-1, -1};
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[end] == target) {
+            res[1] = end;
+        } else if (nums[start] == target) {
+            res[1] = start;
+        }
+        
+        start = 0;
+        end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid;
+            }else {
+                start = mid;
+            }
+        
+        }
+        if (nums[start] == target) {
+            res[0] = start;
+        } else if (nums[end] == target) {
+            res[0] = end;
+        }
+        return res;
+    }
+}
