c0920e25f6342d909118f5198961f57948d24283
wfyin <wf.yin12@gmail.com>
2021-10-30 15:09:54 -0700

    Create wf-lc75.java

---
+++ new/2021/10/27/wf-lc75.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(nums, left, i);
+                left++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, right, i);
+                right--;
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
