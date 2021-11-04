13c34ab242be58c3a796f60dca7aaf7a86d84899
Jian Li <whujianli@gmail.com>
2021-11-03 21:18:30 -0700

    Create jian-lc328.java

---
+++ new/2021/11/03/jian-lc328.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        
+        
+        ListNode oddTail = head;
+        ListNode evenTail = head.next;
+        ListNode evenStart = evenTail;
+        
+        ListNode odd = head.next.next;
+        oddTail.next = null;
+        evenTail.next = null;
+
+        while (odd != null) {
+            ListNode even = odd.next;
+            
+            ListNode nextOddTail = oddTail.next;
+            oddTail.next = odd;
+            odd.next = nextOddTail;
+            oddTail = oddTail.next;
+            
+            if (even == null) {
+                evenTail = null;
+                break;
+            }
+            
+            ListNode nextEven = even.next;
+            ListNode nextEvenTail = evenTail.next;
+            evenTail.next = even;
+            even.next = nextEvenTail;
+            evenTail = evenTail.next;
+            
+            odd = nextEven;
+        }
+        
+        oddTail.next = evenStart;
+        
+        return head;
+    }
+}
