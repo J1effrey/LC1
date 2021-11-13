4b3f1c3943b66bdd2fbf69e6107b3dc25d363e05
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-13 14:57:32 -0800

    Create yyf-lc240.java

---
+++ new/2021/11/13/yyf-lc240.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else if (matrix[row][col] < target) {
+                row++;
+            }
+        }
+        
+        return false;
+    }
+}
