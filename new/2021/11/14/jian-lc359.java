4bf1371276119443ff4e4734ab77b130e1a54c4a
Jian Li <whujianli@gmail.com>
2021-11-14 21:20:42 -0800

    Create jian-lc359.java

---
+++ new/2021/11/14/jian-lc359.java
@@ -0,0 +1,21 @@
+class Logger {
+    Map<String, Integer> m;
+    public int COOL_DOWN_TIME = 10;
+    public Logger() {
+        m = new HashMap<String, Integer>();    
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!m.containsKey(message)) {
+            m.put(message, timestamp);
+            return true;
+        }
+        
+        if (timestamp - m.get(message) >= COOL_DOWN_TIME) {
+            m.put(message, timestamp);
+            return true;
+        }
+        
+        return false;
+    }
+}
