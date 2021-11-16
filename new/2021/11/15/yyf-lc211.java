7608794b8dbfcefb6edc9c301c2d93b0bb7d3d53
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-15 20:45:36 -0800

    Create yyf-lc211.java

---
+++ new/2021/11/15/yyf-lc211.java
@@ -0,0 +1,58 @@
+class WordDictionary {
+    
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+        public TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+    TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        return searchWithDot(word, root);
+    }
+    
+    public boolean searchWithDot(String word, TrieNode nextNode) {
+        TrieNode curr = nextNode;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') {
+                for (int j = 0; j < 26; j++) {
+                    if (curr.children[j] != null) {
+                        if (searchWithDot(word.substring(i+1), curr.children[j])) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            } else if (curr.children[c - 'a'] == null){
+                return false;
+            } else {
+                curr = curr.children[c - 'a'];
+            }
+        }
+        return curr.isEnd;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
