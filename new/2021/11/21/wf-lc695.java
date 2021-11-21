67edf325b943ca65033c0c9db51ae30346a7a6a5
wfyin <wf.yin12@gmail.com>
2021-11-21 10:12:32 -0800

    Create wf-lc695.java

---
+++ new/2021/11/21/wf-lc695.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int res = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                res = Math.max(res, dfs(grid, i, j));
+            }
+        }
+        return res;
+    }
+    
+    private int dfs(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + dfs(grid, i + 1, j)
+                + dfs(grid, i - 1, j)
+                + dfs(grid, i, j + 1)
+                + dfs(grid, i, j - 1);
+        }
+        return 0;
+    }
+}
