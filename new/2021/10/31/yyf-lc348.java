a5c047fe95dc5e306087eb4d38b5da3364fb34c2
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-22 10:48:13 -0800

    Update yyf-lc348.java

--- new/2021/10/31/yyf-lc348.java
+++ new/2021/10/31/yyf-lc348.java
@@ -2,6 +2,59 @@ class TicTacToe {
     int[] rows;
     int[] cols;
     int diagonal;
+    int antiDiagonal;
+    int n;
+    
+    public TicTacToe(int n) {
+        this.rows = new int[n];
+        this.cols = new int[n];
+        this.diagonal = 0;
+        this.antiDiagonal = 0;
+        this.n = n;
+    }
+    
+    public int move(int row, int col, int player) {
+        int val = player == 1 ? 1 : -1;
+        
+        rows[row] += val;
+        if (Math.abs(rows[row]) == n) {
+            return player;
+        }
+        
+        cols[col] += val;
+        if (Math.abs(cols[col]) == n) {
+            return player;
+        }
+        
+        if (row == col) {
+            diagonal += val;
+            if (Math.abs(diagonal) == n) {
+                return player;
+            }
+        }
+        
+        if (row + col == n - 1) {
+            antiDiagonal += val;
+            if (Math.abs(antiDiagonal) == n) {
+                return player;
+            }
+        }   
+        
+        return 0;
+    }
+}
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
+
+
+/*
+class TicTacToe {
+    int[] rows;
+    int[] cols;
+    int diagonal;
     int antidiagonal;
     public TicTacToe(int n) {
         rows = new int[n];
@@ -34,9 +87,4 @@ class TicTacToe {
         }
     }
 }
-
-/**
- * Your TicTacToe object will be instantiated and called as such:
- * TicTacToe obj = new TicTacToe(n);
- * int param_1 = obj.move(row,col,player);
- */
+*/
