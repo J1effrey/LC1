273b9c1070a01d5d848db362651247fd2a537bf1
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-24 08:54:24 -0800

    Update wf-lc150.java

--- new/2021/11/01/wf-lc150.java
+++ new/2021/11/01/wf-lc150.java
@@ -31,3 +31,35 @@ class Solution {
         return stack.pop();
     }
 }
+
+/*
+class Solution {
+    public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            switch(s) {
+                case "+" :
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-" :
+                    stack.push(-stack.pop() + stack.pop());
+                    break;
+                case "*" :
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int after = stack.pop();
+                    int before = stack.pop();
+                    stack.push(before / after);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
+*/
