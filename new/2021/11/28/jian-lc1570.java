9b9411679258ac113c7e381298234f602c15fa18
Jian Li <whujianli@gmail.com>
2021-11-28 21:11:08 -0800

    Create jian-lc1570.java

---
+++ new/2021/11/28/jian-lc1570.java
@@ -0,0 +1,28 @@
+class SparseVector {
+    Map<Integer, Integer> m = new HashMap<>();
+    SparseVector(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            m.put(i, nums[i]);
+        }
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int res = 0;
+        
+        for (Integer index : this.m.keySet()) {
+            if (vec.m.containsKey(index)) {
+                res += this.m.get(index) * vec.m.get(index);
+            }
+        }
+        
+        return res;
+    }
+}
