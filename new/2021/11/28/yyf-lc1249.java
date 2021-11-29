de9a4c6917567c65390d4920bf3b881c349dca02
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-28 22:45:11 -0800

    Update yyf-lc1249.java

--- new/2021/11/28/yyf-lc1249.java
+++ new/2021/11/28/yyf-lc1249.java
@@ -1,6 +1,54 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         if (s == null || s.length() == 0) {
+            return s;
+        }
+        
+        char[] result = new char[s.length()];
+        Stack<Integer> leftBrackets = new Stack<Integer>();
+        
+        for (int i = 0; i <= s.length() - 1; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                result[i] = c;
+                leftBrackets.push(i);
+            } else if (c == ')') {
+                if (!leftBrackets.empty()) {
+                    result[i] = c;
+                    leftBrackets.pop();
+                } else {
+                    result[i] = '.';
+                }
+            } else {
+                result[i] = c;
+            }
+        }
+        
+        while (!leftBrackets.isEmpty()) {
+            int leftBracketToRemove = leftBrackets.pop();
+            result[leftBracketToRemove] = '.';
+        }
+        
+        return convertCharArrayToAnswer(result);
+    }
+    
+    private String convertCharArrayToAnswer(char[] c) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i <= c.length - 1; i++) {
+            if (c[i] != '.') {
+                sb.append(c[i]);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+
+/*
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if (s == null || s.length() == 0) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
@@ -34,6 +82,7 @@ class Solution {
         return res.toString();
     }
 }
+*/
 /*
 (( )))
 
