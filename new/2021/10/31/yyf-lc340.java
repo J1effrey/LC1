87b03a735d9d1cf4e982d0cc94624031c2fd2338
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-11-30 09:09:39 -0800

    Update yyf-lc340.java

--- new/2021/10/31/yyf-lc340.java
+++ new/2021/10/31/yyf-lc340.java
@@ -1,5 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (s == null || s.length() == 0 || k <= 0) {
+            return 0;
+        }
+        
+        Map<Character, Integer> appearance = new HashMap<>();
+        int distinctNum = 0;
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        
+        while (right < s.length()) {
+            char cur = s.charAt(right);
+            appearance.put(cur, appearance.getOrDefault(cur, 0) + 1);
+            if (appearance.get(cur) == 1) {
+                distinctNum++;
+            }
+            
+            while (distinctNum > k) {
+                char pre = s.charAt(left);
+                int count = appearance.get(pre);
+                appearance.put(pre, count - 1);
+                if (appearance.get(pre) == 0) {
+                    distinctNum--;
+                }
+                left++;
+            }
+            res = Math.max(res, right - left + 1);
+            right++;
+        }
+        
+        return res;
+    }
+}
+// ======================================================================================================
+/*
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
         Map<Character, Integer> appearance = new HashMap<>();
         int unique = 0;
         int left = 0;
@@ -31,7 +68,8 @@ class Solution {
         return max;
     }
 }
-
+*/
+// ======================================================================================================
 /*
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
