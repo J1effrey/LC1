5593c95252d155258af83df10ac3c39d4d2b47ba
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-05 10:37:04 -0800

    Create yyf-lc67.java

---
+++ new/2021/12/05/yyf-lc67.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String addBinary(String a, String b) {
+       if (a == null || b == null || a.length() == 0 || b.length() == 0) {
+           return "";
+       } 
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int x = i < 0 ? 0 : a.charAt(i) - '0';
+            int y = j < 0 ? 0 : b.charAt(j) - '0';
+            sb.append((x + y + carry) % 2);
+            carry = (x + y + carry) / 2;
+            i--;
+            j--;
+        }
+        if (carry > 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
