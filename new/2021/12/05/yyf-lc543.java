9e385e134ebae1ad5c032c9bbed508f672ffc3e9
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-05 10:37:16 -0800

    Create yyf-lc543.java

---
+++ new/2021/12/05/yyf-lc543.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+    
+    public int maxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = maxDepth(node.left);
+        int right = maxDepth(node.right);
+        max = Math.max(max, left + right);
+        return 1 + Math.max(left, right);
+    }
+}
