c2d841f9b833cac53fbde4f2e05d85f7eaf30026
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-20 10:48:08 -0800

    Update yyf-lc545.java

--- new/2021/11/17/yyf-lc545.java
+++ new/2021/11/17/yyf-lc545.java
@@ -13,6 +13,68 @@
  *     }
  * }
  */
+
+class Solution {
+    List<Integer> nodes;
+    
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        nodes = new ArrayList<>();
+        
+        if (root == null) {
+            return nodes;
+        }
+
+        nodes.add(root.val);
+        addLeftBoundary(root.left);
+        addLeaves(root.left);
+        addLeaves(root.right);
+        addRightBoundary(root.right);
+
+        return nodes;
+    }
+    
+    public void addLeftBoundary(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return;
+        }
+        
+        nodes.add(root.val);
+        if (root.left == null) {
+            addLeftBoundary(root.right);
+            return;
+        }
+        
+        addLeftBoundary(root.left);
+    }
+    
+    public void addRightBoundary(TreeNode root) {
+        if (root == null || (root.right == null && root.left == null)) {
+            return;
+        }
+        if (root.right == null) {
+            addRightBoundary(root.left);
+        } else {
+            addRightBoundary(root.right);
+        }
+        nodes.add(root.val); // add after child visit(reverse)
+    }
+    
+    public void addLeaves(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        if (root.left == null && root.right == null) {
+            nodes.add(root.val);
+            return;
+        }
+        
+        addLeaves(root.left);
+        addLeaves(root.right);
+    }
+}
+
+/*
 class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -43,7 +105,7 @@ class Solution {
         }
      }
 }
-
+*/
 /*
 1 2 4 7 8 
 
