19c154aae94946065c171f70150fdb3bb401a39c
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-20 11:33:44 -0800

    Update yyf-lc460.java

--- new/2021/12/19/yyf-lc460.java
+++ new/2021/12/19/yyf-lc460.java
@@ -1,99 +1,76 @@
-class RangeModule {
-    TreeMap<Integer, Integer> treeMap;
-    public RangeModule() {
-        treeMap = new TreeMap<>();
+class LFUCache {
+    Map<Integer, Integer> vals;
+    Map<Integer, Integer> counts;
+    Map<Integer, LinkedHashSet<Integer>> list; 
+    int minCount = 1;
+    int capacity;
+
+    public LFUCache(int capacity) {
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        list = new HashMap<>();
+        this.capacity = capacity;
+        list.put(1, new LinkedHashSet<>());
+    }
+    /*
+    ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+    [[2],       [1, 1], [2, 2], [1], [3, 3],  [2],   [3],  [4, 4], [1],   [3],   [4]]
+    vals:  3:3 4:4
+    count: 1:2 2:1 3:3 4:2
+    list: 1:[] 2:[4] 3:[3]
+    minCount:2
+    capacity: 2
+    deleteKey: 2
+    */
+    public int get(int key) {
+        if (!vals.containsKey(key)) {
+            return -1;
+        }
+        int count = counts.get(key);
+        counts.put(key, count + 1);
+        list.get(count).remove(key);
+        if (!list.containsKey(count + 1)) {
+            list.put(count + 1, new LinkedHashSet<>());
+        }
+        list.get(count + 1).add(key);
+        if (count == minCount && list.get(count).size() == 0) {
+            minCount  = count + 1;
+        }
+        return vals.get(key);
     }
     
-    public void addRange(int left, int right) {
-        while (treeMap.floorEntry(right) != null) {
-            Map.Entry<Integer, Integer> entry = treeMap.floorEntry(right);
-            if (entry.getValue() < left) {
-                break;
-            }
-            left = Math.min(left, entry.getKey());
-            right = Math.max(right, entry.getValue());
-            treeMap.remove(entry.getKey());
+    public void put(int key, int value) {
+        if (capacity <= 0) {
+            return;
         }
-        treeMap.put(left, right);
-    }
-    
-    public boolean queryRange(int left, int right) {
-        Map.Entry<Integer, Integer> entry = treeMap.floorEntry(left);
-        return entry != null && entry.getValue() >= right;
-    }
-    
-    public void removeRange(int left, int right) {
-        addRange(left, right);
-        Map.Entry<Integer, Integer> entry = treeMap.floorEntry(left);
-        treeMap.remove(entry.getKey());
-        if (entry.getKey() < left) {
-            treeMap.put(entry.getKey(), left);
+        if (vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key);
+            return;
         }
-        if (entry.getValue() > right) {
-            treeMap.put(right, entry.getValue());
+        if (vals.size() >= capacity) {
+            int deleteKey = list.get(minCount).iterator().next();
+            vals.remove(deleteKey);
+            counts.remove(deleteKey);
+            list.get(minCount).remove(deleteKey);
         }
+        vals.put(key, value);
+        counts.put(key, 1);
+        minCount = 1;
+        list.get(1).add(key);
     }
 }
 
 /**
- * Your RangeModule object will be instantiated and called as such:
- * RangeModule obj = new RangeModule();
- * obj.addRange(left,right);
- * boolean param_2 = obj.queryRange(left,right);
- * obj.removeRange(left,right);
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
  */
 
 /*
-10 20
-10:14 16:20
-
-*/class RangeModule {
-    TreeMap<Integer, Integer> treeMap;
-    public RangeModule() {
-        treeMap = new TreeMap<>();
-    }
-    
-    public void addRange(int left, int right) {
-        while (treeMap.floorEntry(right) != null) {
-            Map.Entry<Integer, Integer> entry = treeMap.floorEntry(right);
-            if (entry.getValue() < left) {
-                break;
-            }
-            left = Math.min(left, entry.getKey());
-            right = Math.max(right, entry.getValue());
-            treeMap.remove(entry.getKey());
-        }
-        treeMap.put(left, right);
-    }
-    
-    public boolean queryRange(int left, int right) {
-        Map.Entry<Integer, Integer> entry = treeMap.floorEntry(left);
-        return entry != null && entry.getValue() >= right;
-    }
-    
-    public void removeRange(int left, int right) {
-        addRange(left, right);
-        Map.Entry<Integer, Integer> entry = treeMap.floorEntry(left);
-        treeMap.remove(entry.getKey());
-        if (entry.getKey() < left) {
-            treeMap.put(entry.getKey(), left);
-        }
-        if (entry.getValue() > right) {
-            treeMap.put(right, entry.getValue());
-        }
-    }
-}
-
-/**
- * Your RangeModule object will be instantiated and called as such:
- * RangeModule obj = new RangeModule();
- * obj.addRange(left,right);
- * boolean param_2 = obj.queryRange(left,right);
- * obj.removeRange(left,right);
- */
-
-/*
-10 20
-10:14 16:20
-
+vals
+counts
+list: count -> LinkedHashSet<> keys
+minCount = 1;
 */
