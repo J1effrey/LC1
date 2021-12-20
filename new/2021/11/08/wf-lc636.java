06beb2a2cbe1b2f42bd541ed42d1330bad640eb9
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-20 11:07:39 -0800

    Update wf-lc636.java

--- new/2021/11/08/wf-lc636.java
+++ new/2021/11/08/wf-lc636.java
@@ -1,4 +1,45 @@
 class Solution {
+    static class Log {
+        public int id;
+        public boolean isStart;
+        public int time;
+        
+        public Log(String content) {
+            String[] strs = content.split(":");
+            id = Integer.valueOf(strs[0]);
+            isStart = strs[1].equals("start");
+            time = Integer.valueOf(strs[2]);
+        }
+    }
+    
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        
+        if (logs == null || logs.size() == 0) {
+            return result;
+        }
+        
+        Stack<Log> stack = new Stack<>();
+        
+        for (String content : logs) {
+            Log log = new Log(content);
+            if (log.isStart) {
+                stack.push(log);
+            } else {
+                Log top = stack.pop();
+                result[top.id] += (log.time - top.time + 1);
+                if (!stack.isEmpty()) {
+                    // very good
+                    result[stack.peek().id] -= (log.time - top.time + 1);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+/*
+class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         if (logs == null || logs.size() == 0 || n == 0) {
             return new int[]{};
@@ -30,3 +71,4 @@ class Solution {
         return res;
     }
 }
+*/
