75ddb8848c71b8ec35e42c19b0af301920b21305
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-22 08:58:18 -0800

    Update jian-lc323.java

--- new/2021/11/21/jian-lc323.java
+++ new/2021/11/21/jian-lc323.java
@@ -1,60 +1,60 @@
 class Solution {
-    class UnionFind {
-        private int[] parents;
-        private int[] length;
-        
-        public UnionFind(int size) {
-            parents = new int[size];
-            length = new int[size];
-            for (int i = 0; i < size; i++) {
-                parents[i] = i;
-                length[i] = 1;
+    class Union {
+        int[] parent;
+        int[] size;
+        int count;
+        public Union(int n) {
+            this.parent = new int[n];
+            this.size = new int[n];
+            this.count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
         
-        public void union(int a, int b) {
-            int aParent = find(a);
-            int bParent = find(b);
-            if (aParent == bParent) {
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) {
                 return;
-            }
+            } 
             
-            if (length[aParent] < length[bParent]) {
-                parents[aParent] = bParent;
-            } else if (length[aParent] > length[bParent]) {
-                parents[bParent] = aParent;
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
             } else {
-                parents[aParent] = bParent;
-                length[bParent] += 1;
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
             }
+            count--;
         }
         
-        public int find(int a) {
-            while (a != parents[a]) {
-                a = parents[a];
-            }
-            return a;
+        public int getCount() {
+            return this.count;
         }
     }
-    
     public int countComponents(int n, int[][] edges) {
-        if (n <= 0 || edges == null || edges.length == 0 || edges[0] == null || edges[0].length == 0) {
-            return n;
+        if (edges == null) {
+            return 0;
         }
-        
-        UnionFind uf = new UnionFind(n);
-        
-        for (int[] edge: edges) {
-            uf.union(edge[0], edge[1]);
+        Union uf = new Union(n);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < edges.length; i++) {
+            uf.union(edges[i][0], edges[i][1]);
         }
-        
-        Set<Integer> parents = new HashSet<Integer>();
-        
-        for (int i = 0; i < n; i++) {
-            parents.add(uf.find(i));
-        }
-        
-        return parents.size();
+        // for (int i = 0; i < n; i++) {
+        //     set.add(uf.find(i));
+        // }
+        return uf.getCount();
         
     }
 }
