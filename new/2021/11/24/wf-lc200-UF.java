6960d5913f565192ddfed968d7471b3a6a8ea659
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-22 17:23:16 -0800

    Update wf-lc200-UF.java

--- new/2021/11/24/wf-lc200-UF.java
+++ new/2021/11/24/wf-lc200-UF.java
@@ -1,3 +1,84 @@
+// without set
+class Solution {
+    class Union {
+        int[] parent;
+        int[] size;
+        int count;
+        public Union(int n) {
+            this.parent = new int[n];
+            this.size = new int[n];
+            this.count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            } 
+            
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+            }
+            count--;
+        }
+        
+        public int getCount() {
+            return this.count;
+        }
+    }
+    
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int zeroCount = 0;
+        Union uf = new Union(m * n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0') {
+                    zeroCount++;
+                    continue;
+                }
+                if (i > 0 && grid[i-1][j] == '1') {
+                    uf.union(i * n + j, (i - 1) * n + j);
+                }
+                if (i < m - 1 && grid[i + 1][j] == '1') {
+                    uf.union(i * n + j, (i + 1) * n + j);
+                }
+                if (j > 0 && grid[i][j - 1] == '1') {
+                    uf.union(i * n + j, i * n + j - 1);
+                }
+                if (j < n - 1 && grid[i][j + 1] == '1') {
+                    uf.union(i * n + j, i * n + j + 1);
+                }
+            }
+        }
+        return uf.getCount() - zeroCount;
+    }
+}
+
+/*=============================================================================================================================================================================*/
+
+//  with set
 class Solution {
     class UnionFind {
         private int[] pa;
@@ -75,3 +156,4 @@ class Solution {
         
     }
 }
+
