071744d7fe0442e5b7792e67d175e20c550640d8
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-27 09:48:50 -0800

    Update yyf-lc471.java

--- new/2021/12/06/yyf-lc471.java
+++ new/2021/12/06/yyf-lc471.java
@@ -1,4 +1,30 @@
 class Solution {
+    public String encode(String s) {
+        int n = s.length();
+    	String[][] dp = new String[n][n];
+    	for(int len = 1; len <= n; ++len) {		//枚举区间长度
+    		for(int i = 0; i + len - 1 < n; ++i) {		//枚举起点
+    			int j = i + len - 1;					//计算终点
+    			dp[i][j] = s.substring(i,  j + 1);
+    			for(int k = i; k < j; ++k) {			//利用k分割出左部分和右部分
+    				if(dp[i][k].length() + dp[k + 1][j].length() < dp[i][j].length()) {  //如果左右两侧长度和小于当前答案就更新
+    					dp[i][j] = dp[i][k] + dp[k + 1][j];
+    				}
+    			}
+    			String now = s.substring(i, j + 1);  	//截取当前区间
+    			int st = (now + now).indexOf(now, 1);  //1表示从拼接的字符串首字母后开始查找
+    			if(st < now.length()) {    //如果区间内包含重复字符串，则返回的起点不会超过区间
+                    now = String.valueOf(now.length() / st) + "[" + dp[i][i + st - 1] + "]";    //计算字符串前的数字即题面中的k
+                }
+    			if(now.length() < dp[i][j].length())dp[i][j] = now;  //如果长度更小就进行更新
+    		}
+    	}
+    	return dp[0][n - 1];
+    }
+}
+
+
+class Solution {
     Map<String, String> map = new HashMap<>();
     public String encode(String s) {
         if (s == null || s.length() == 0) {
