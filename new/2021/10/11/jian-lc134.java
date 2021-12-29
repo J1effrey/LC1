13c13923f366197ab7eabf83ccc7b5f4b17d16f6
Jian Li <whujianli@gmail.com>
2021-12-29 09:24:22 -0800

    Update jian-lc134.java

--- new/2021/10/11/jian-lc134.java
+++ new/2021/10/11/jian-lc134.java
@@ -25,3 +25,35 @@ class Solution {
         return total + deficit >= 0 ? start : -1;
     }
 }
+
+==========
+// same as above, use your own judgement to choose the one you like during interview    
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || gas.length == 0) {
+            return -1;
+        }
+        
+        if (cost == null || cost.length == 0) {
+            return 0;
+        }
+        
+        int debt = 0;
+        int remain = 0;
+        int startIndex = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            int newRemain = remain + gas[i] - cost[i];
+            if (newRemain < 0) {
+                debt += -newRemain;
+                startIndex = i + 1;
+                remain = 0;
+            } else {
+                remain = newRemain;
+            }
+        }
+        
+        
+        return remain >= debt ? startIndex : -1;
+    }
+}
