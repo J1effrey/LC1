9a1a2ce2bab4b3d281ff004436d74402ac36051a
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-30 09:17:02 -0800

    Update yyf-lc460.java

--- new/2021/12/19/yyf-lc460.java
+++ new/2021/12/19/yyf-lc460.java
@@ -1,3 +1,125 @@
+// doubleLinkedList
+class LFUCache {
+    class Node {
+        int key;
+        int val;
+        int freq;
+        Node prev;
+        Node next;
+        Node (int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.freq = 1;
+            prev = null;
+            next = null;
+        }
+    }
+    
+    class DoubledLinkedList {
+        Node head;
+        Node tail;
+        int size;
+        
+        DoubledLinkedList() {
+            size = 0;
+            head = new Node(-1, -1);
+            tail = new Node(-1, -1);
+            head.next = tail;
+            tail.prev = head;
+        }
+        
+        public void add(Node node) { // add to head
+            if (node == null) {
+                return;
+            }
+            // head node next tail
+            Node next = head.next;
+            node.next = next;
+            node.prev = next.prev;
+            head.next = node;
+            next.prev = node;
+            size++;
+        }
+        
+        public void remove(Node node) {
+            if (node == null) {
+                return;
+            }
+            // head ... prev node next ... tail
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+            size--;
+        }
+        
+        public Node removeLast() {
+            if (size > 0) {
+                Node node = tail.prev;
+                remove(node);
+                return node;
+            }
+            else return null;
+        }
+    }
+    
+    private int capacity;
+    private int min = 1;
+    private Map<Integer, Node> nodeMap;
+    private Map<Integer, DoubledLinkedList> freqMap; // freq map
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        nodeMap = new HashMap<>();
+        freqMap = new HashMap<>();
+    }
+    
+    public int get(int key) {
+        if (nodeMap.containsKey(key)) {
+            addFreq(nodeMap.get(key));
+            return nodeMap.get(key).val;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+        if (!nodeMap.containsKey(key)) {
+            Node newNode = new Node(key, value);
+            nodeMap.put(key, newNode);
+            if (nodeMap.size() > capacity) {
+                Node removedNode = freqMap.get(min).removeLast();
+                nodeMap.remove(removedNode.key);
+            }
+            DoubledLinkedList list = freqMap.getOrDefault(newNode.freq, new DoubledLinkedList());
+            list.add(newNode);
+            freqMap.put(newNode.freq, list);
+            min = 1;
+        } else {
+            Node node = nodeMap.get(key);
+            node.val = value;
+            addFreq(nodeMap.get(key));
+        }
+    }
+    
+    public void addFreq(Node node) { // node.freq must exist, no check
+        DoubledLinkedList oldList = freqMap.get(node.freq);
+        oldList.remove(node);
+        if (min == node.freq && oldList.size == 0) {
+            min++;
+        }
+        node.freq++;
+        DoubledLinkedList newList = freqMap.getOrDefault(node.freq, new DoubledLinkedList());
+        newList.add(node);
+        freqMap.put(node.freq, newList);
+    }
+}
+
+
+/*=============================================================================================================================================================================*/
+// linkedHashSet
 class LFUCache {
     Map<Integer, Integer> vals;
     Map<Integer, Integer> counts;
@@ -12,16 +134,7 @@ class LFUCache {
         this.capacity = capacity;
         list.put(1, new LinkedHashSet<>());
     }
-    /*
-    ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
-    [[2],       [1, 1], [2, 2], [1], [3, 3],  [2],   [3],  [4, 4], [1],   [3],   [4]]
-    vals:  3:3 4:4
-    count: 1:2 2:1 3:3 4:2
-    list: 1:[] 2:[4] 3:[3]
-    minCount:2
-    capacity: 2
-    deleteKey: 2
-    */
+
     public int get(int key) {
         if (!vals.containsKey(key)) {
             return -1;
