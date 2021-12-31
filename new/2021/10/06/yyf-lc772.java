df45ca289846bc1ba926f772d4da51397f91480e
Jian Li <whujianli@gmail.com>
2021-12-30 23:28:25 -0800

    Update yyf-lc772.java

--- new/2021/10/06/yyf-lc772.java
+++ new/2021/10/06/yyf-lc772.java
@@ -3,56 +3,60 @@ class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
+        Queue<Character> q = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            q.offer(c);
+        }
+        return helper(q);
+    }
+    
+    public int helper(Queue<Character> q) {
         
-        char[] ss = s.toCharArray();
-        Stack<Integer> stack = new Stack<>();
-        char op = '+';
-        int currentVal = 0;
-        for (int i = 0; i < ss.length; i++) {
-            if (Character.isDigit(ss[i])) {
-                currentVal = currentVal * 10 + ss[i] - '0';
+        Stack<Integer> stack = new Stack<Integer>();
+        int operand = 0;
+        char operation = '+';
+        
+        while (!q.isEmpty()) {
+            char c = q.poll();
+            if (Character.isDigit(c)) {
+                operand = operand * 10 + c - '0';    
             }
             
-            if (!Character.isDigit(ss[i]) && !Character.isWhitespace(ss[i]) || i == ss.length - 1) {
-                if (ss[i] == '(') {
-                    int start = i + 1;
-                    int end = findFirstMatchBracket(ss, 1, 0, i + 1);
-                    currentVal = calculate(s.substring(start, end));
-                    i = end - 1;
-                    continue;
-                } else if (op == '+') {
-                    stack.push(currentVal);
-                } else if (op == '-') {
-                    stack.push(-currentVal);
-                } else if (op == '*') {
-                    stack.push(stack.pop() * currentVal);
-                } else if (op == '/') {
-                    stack.push(stack.pop() / currentVal);
+            if (c == '(') {
+                operand = helper(q);
+            }
+            
+            if (!Character.isDigit(c) || q.isEmpty()) {
+                if (operation == '+') {
+                    stack.push(operand);
+                } else if (operation == '-') {
+                    stack.push(-operand);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * operand);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / operand);
                 }
-                op = ss[i];
-                currentVal = 0;
+                operand = 0;
+                operation = c;
             }
             
+            if (c == ')') {
+                break;
+            }
         }
         
         int res = 0;
-        while (!stack.isEmpty()) {
+        while (!stack.empty()) {
             res += stack.pop();
         }
-        
         return res;
     }
-    
-    private int findFirstMatchBracket(char[] ss, int open, int close, int index) {
-        while (open > close) {
-            if (ss[index] == '(') {
-                open++;
-            } else if (ss[index] == ')') {
-                close++;
-            }
-            index++;
-        }
-        
-        return index - 1;
-    }
 }
+
+/*
+
+TDD : Test Driven Development
+
+"6-4/2"
+"2*(5+5*2)/3+(6/2+8)"
+*/
