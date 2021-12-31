1839518d73720b317653c1926f7aa746ea4457d2
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2021-12-31 08:53:30 -0800

    Update wf-lc1423.java

--- new/2021/12/19/wf-lc1423.java
+++ new/2021/12/19/wf-lc1423.java
@@ -1,3 +1,4 @@
+// O(K)
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int frontScore = 0;
@@ -22,3 +23,25 @@ class Solution {
         return maxScore;
     }
 }
+
+// sliding window
+// O(N)
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int res = 0, len = cardPoints.length;
+        for (int start = len - k, i = start, win = 0; i < len + k; ++i) {
+            win += cardPoints[i % len]; // accumulate the value of the sliding window.
+            if (i - start >= k) { // Is the sliding window wider than k?
+                win -= cardPoints[(i - k) % len]; // deduct the element from the left of the window.
+            }
+            res = Math.max(win, res); // update the maximum.
+        }
+        return res;
+    }
+}
+
+/*
+1 2 3 4 [5 6 1 1 2 3] 4 5 6 1
+
+len - k  k
+*/
