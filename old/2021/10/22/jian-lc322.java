5e74a68d7fca4a63b0d7eef289e9239ec468e8c4
Jian Li <whujianli@gmail.com>
2021-12-31 14:36:06 -0800

    Update jian-lc322.java

--- old/2021/10/22/jian-lc322.java
+++ old/2021/10/22/jian-lc322.java
@@ -1,3 +1,30 @@
+// Time:  O(amount * num-coins)
+// Space: o(amount), We use extra space for the memoization table.
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }   
+            }
+        }
+        
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
+
+---------------------
+    
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if (coins == null || coins.length == 0) {
