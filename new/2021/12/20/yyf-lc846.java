83e4216ac11653aa29f44f146ccc1f6888a58881
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-02 10:32:46 -0800

    Update yyf-lc846.java

--- new/2021/12/20/yyf-lc846.java
+++ new/2021/12/20/yyf-lc846.java
@@ -1,32 +1,24 @@
+// Time: O((N+MK)logM)  N是hand长度 M是不同的卡片个数 k是groupSize
+// Space: O(M)
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if (hand == null || hand.length == 0 || hand.length % groupSize != 0) {
             return false;
         }
         Map<Integer, Integer> map = new TreeMap<>();
-        for (int i : hand) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        for (int i : hand) { // O(N)
+            map.put(i, map.getOrDefault(i, 0) + 1); // O(logM)
         }
-        for (int i : map.keySet()) {
-            if (map.get(i) <= 0) {
-                continue;
-            }
-            for (int j = groupSize - 1; j >= 0; j--) {
-                if (map.get(i) > map.getOrDefault(i + j, 0)) {
-                    return false;
+        for (int i : map.keySet()) { // O(M)
+            if (map.get(i) > 0) { 
+                for (int j = groupSize - 1; j >= 0; j--) { // O(K)
+                    if (map.get(i) > map.getOrDefault(i + j, 0)) {
+                        return false;
+                    }
+                    map.put(i + j, map.get(i + j) - map.get(i)); // O(logM)
                 }
-                map.put(i + j, map.get(i + j) - map.get(i));
             }
         }
         return true;
     }
 }
-
-/*
-1,2,3,5 2
-
-1 0
-2 0
-3 1
-5 1
-*/
