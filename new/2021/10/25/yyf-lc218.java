1000d8050e4cc57660254d12b70ddc4b7d9f6b0a
Jian Li <whujianli@gmail.com>
2022-01-06 21:10:00 -0800

    Update yyf-lc218.java

--- new/2021/10/25/yyf-lc218.java
+++ new/2021/10/25/yyf-lc218.java
@@ -1,26 +1,41 @@
+// T: O(N * N)
+// S: O(N)
+
 class Solution {
-    private class Point implements Comparable<Point>{
-        private int x;
-        private int height;
-        private boolean isStart;
+    /*
+    if a point should go to the result?
+     -> if the point changes the current max height
+    
+    1. maintain cur max height : PriorityQueue, maxHeap, reverseOrder
+    
+        differentiate start and end point, 
+        if I meet end point, should have the start point left, and if start point changes max height, I will need to change max height.
+    
+    2. find the breakers that changes max height
+    */
+    class Point implements Comparable<Point> {
+        int x;
+        int height;
+        boolean isStart;
         
         public Point(int x, int height, boolean isStart) {
             this.x = x;
             this.height = height;
             this.isStart = isStart;
         }
-
+        
+        @Override
         public int compareTo(Point p) {
             if (this.x != p.x) {
                 return this.x - p.x;
             } else {
                 if (this.isStart && p.isStart) {
                     return p.height - this.height;
-                } 
+                }
                 if (!this.isStart && !p.isStart) {
                     return this.height - p.height;
                 }
-                return this.isStart? -1 : 1;
+                return this.isStart ? -1 : 1;
             }
         }
     }
@@ -28,59 +43,54 @@ class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         
-        int len = buildings.length;
-
-        if (buildings == null || len == 0 || buildings[0].length == 0) {
+        if (buildings == null || buildings.length == 0 || buildings[0] == null || buildings[0].length == 0) {
             return res;
         }
         
-        Point[] points = new Point[len * 2];
+        Point[] points = new Point[2 * buildings.length];
         int index = 0;
-
         for (int[] building : buildings) {
             int start = building[0];
             int end = building[1];
-            int h = building[2];
-            points[index++] = new Point(start, h, true);
-            points[index++] = new Point(end, h, false);
-            
+            int height = building[2];
+            points[index++] = new Point(start, height, true);
+            points[index++] = new Point(end, height, false);
         }
         
         Arrays.sort(points);
-        
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
         queue.offer(0);
         
-        int prevMaxVal = queue.peek();
-        for (Point point : points) {
+        int preMaxVal = queue.peek();
+        for (Point point: points) { // O(n)
             if (point.isStart) {
                 queue.offer(point.height);
-                if (queue.peek() > prevMaxVal) {
-                    res.add(getOutputPoint(point.x, point.height));
-                    prevMaxVal = queue.peek();
+                if (queue.peek() > preMaxVal) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(point.x);
+                    list.add(queue.peek());
+                    res.add(list);
+                    preMaxVal = queue.peek();
                 }
             } else {
-                queue.remove(point.height);
-                if (queue.peek() < prevMaxVal) {
-                    res.add(getOutputPoint(point.x, queue.peek()));
-                    prevMaxVal = queue.peek();
+                queue.remove(point.height); // O(n)
+                if (queue.peek() < preMaxVal) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(point.x);
+                    list.add(queue.peek());
+                    res.add(list);
+                    preMaxVal = queue.peek();
                 }
-                
             }
         }
+        
         return res;
     }
-    
-    private List<Integer> getOutputPoint(int x, int y) {
-        List<Integer> outputPoint = new ArrayList<Integer>();
-        outputPoint.add(x);
-        outputPoint.add(y);
-        
-        return outputPoint;
-    }
-
 }
 
+
+// T: O(N * logN)
+// S: O(N)
 /*
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
