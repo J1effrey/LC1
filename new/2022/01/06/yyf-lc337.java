59dcf93b4e1cb789036a7e515248a129f0b24bcc
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-06 20:56:08 -0800

    Create yyf-lc337.java

---
+++ new/2022/01/06/yyf-lc337.java
@@ -0,0 +1,32 @@
+// O(N) for all nodes once, and saved the results to prevent the second calculation
+// O(N) we need two maps with the size of O(N) to store the results, and O(N) space for stacks to start recursion.
+class Solution {
+    HashMap<TreeNode, Integer> robResult = new HashMap<>();
+    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
+    public int helper(TreeNode node, boolean parentRobbed) {
+        if (node == null) {
+            return 0;
+        }
+        if (parentRobbed) {
+            if (robResult.containsKey(node)) {
+                return robResult.get(node);
+            }
+            int result = helper(node.left, false) + helper(node.right, false);
+            robResult.put(node, result);
+            return result;
+        } else {
+            if (notRobResult.containsKey(node)) {
+                return notRobResult.get(node);
+            }
+            int rob = node.val + helper(node.left, true) + helper(node.right, true);
+            int notRob = helper(node.left, false) + helper(node.right, false);
+            int result = Math.max(rob, notRob);
+            notRobResult.put(node, result);
+            return result;
+        }
+    }
+    
+    public int rob(TreeNode root) {
+        return helper(root, false);
+    }
+}
