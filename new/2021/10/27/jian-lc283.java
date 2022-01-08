b9a10a34c0435960fa2d24997a7c70c6fae78de9
Jian Li <whujianli@gmail.com>
2022-01-07 20:53:25 -0800

    Update jian-lc283.java

--- new/2021/10/27/jian-lc283.java
+++ new/2021/10/27/jian-lc283.java
@@ -4,6 +4,35 @@ class Solution {
             return;
         }
         
+        int nonZeroP = 0;
+        int zeroP = 0;
+        
+        
+        while (nonZeroP < nums.length) {
+            if (nums[nonZeroP] != 0) {
+                swap(nums, nonZeroP, zeroP);
+                zeroP++;
+            }
+            nonZeroP++;
+        }
+    }
+    
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
+
+
+===========
+    
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
         int windowSize = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
