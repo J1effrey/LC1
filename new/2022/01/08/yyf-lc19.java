e73417633a01a16febf490d3972a35b84de70866
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-08 16:32:47 -0800

    Create yyf-lc19.java

---
+++ new/2022/01/08/yyf-lc19.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
