4a051d3bc25616ae2c8e4aa7d0071647738676ca
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-08 16:23:55 -0800

    Update yyf-lc238.java

--- new/2021/10/02/yyf-lc238.java
+++ new/2021/10/02/yyf-lc238.java
@@ -20,3 +20,27 @@ class Solution {
         return res;
     }
 }
+
+=====================================================================
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        
+        int[] res = new int[nums.length];
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        
+        int postProduct = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = postProduct * left[i];
+            postProduct *= nums[i];
+        }
+        return res;
+    }
+}
