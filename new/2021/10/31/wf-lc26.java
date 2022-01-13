9ed7485ceac3808b3cb99257a05527f1975dd70d
Jian Li <whujianli@gmail.com>
2022-01-13 08:57:33 -0800

    Update wf-lc26.java

--- new/2021/10/31/wf-lc26.java
+++ new/2021/10/31/wf-lc26.java
@@ -1,5 +1,27 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                // 维护 nums[0..slow] 无重复
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        // 数组长度为索引 + 1
+        return slow + 1;
+    }
+}
+
+====
+    
+class Solution {
+    public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
