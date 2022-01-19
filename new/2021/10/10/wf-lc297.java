5b26fff1f8579ffc8684267b291df0761a412d2b
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-18 23:21:55 -0800

    Update wf-lc297.java

--- new/2021/10/10/wf-lc297.java
+++ new/2021/10/10/wf-lc297.java
@@ -1,29 +1,72 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// bfs
+public class Codec {
 
-/*
-    Root -> left Node -> right Node
-    1 -> 2 -> 3
-    
-    1 -> 2 -> null -> null -> 3 -> 4 -> null -> null -> 5 -> null -> null
-String: [1 -> 2 -> null -> null -> 3 -> 4 -> null -> null  5  null  null]
-    root = 1
-    left = [2 -> null -> null -> 3 -> 4 -> null -> null  5  null  null];
-    right = [3 -> 4 -> null -> null  5  null  null]
-        left: 3
-            left 4
-                left: null right: null 
-        right: 5 [5  null  null]
-            left null
-            right null
-*/
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                if (cur == null) {
+                    sb.append('#');
+                } else {
+                    sb.append(cur.val);
+                }    
+                
+                if (cur != null) {
+                    q.offer(cur.left);
+                    q.offer(cur.right);
+                }
+                
+                if (!q.isEmpty()) {
+                    sb.append(',');
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "") {
+            return null;
+        }
+        
+        String[] strs = data.split(",");
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        TreeNode root = new TreeNode(Integer.parseInt(strs[0]));
+        q.offer(root);
+        
+        for (int i = 1; i < strs.length; i++) {
+            TreeNode cur = q.poll();
+            if (!strs[i].equals("#")) {
+                TreeNode left = new TreeNode(Integer.parseInt(strs[i]));
+                cur.left = left;
+                q.add(left);
+            } 
+            i++;
+            if (!strs[i].equals("#")) {
+                TreeNode right = new TreeNode(Integer.parseInt(strs[i]));
+                cur.right = right;
+                q.add(right);
+            }
+        }
+
+        return root;
+    }
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------
+// dfs
 public class Codec {
     private String serialize(TreeNode root) {
         if (root == null) {
@@ -53,6 +96,23 @@ public class Codec {
         return root;
     }
 }
+
+/*
+    Root -> left Node -> right Node
+    1 -> 2 -> 3
+    
+    1 -> 2 -> null -> null -> 3 -> 4 -> null -> null -> 5 -> null -> null
+String: [1 -> 2 -> null -> null -> 3 -> 4 -> null -> null  5  null  null]
+    root = 1
+    left = [2 -> null -> null -> 3 -> 4 -> null -> null  5  null  null];
+    right = [3 -> 4 -> null -> null  5  null  null]
+        left: 3
+            left 4
+                left: null right: null 
+        right: 5 [5  null  null]
+            left null
+            right null
+*/
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
