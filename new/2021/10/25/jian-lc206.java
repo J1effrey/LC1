90ce51cced26ab757942e279dbfd759e48cc393c
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-18 23:30:04 -0800

    Update jian-lc206.java

--- new/2021/10/25/jian-lc206.java
+++ new/2021/10/25/jian-lc206.java
@@ -1,5 +1,23 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        
+        return prev;
+        
+        
+    }
+}
+
+---------------------------------------------
+class Solution {
+    public ListNode reverseList(ListNode head) {
         if (head == null) {
             return head;
         }
