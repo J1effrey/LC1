666e437ade495e0fd0fb9500f1e3d9d5de58938b
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-27 09:18:47 -0800

    Update wf-lc1060.java

--- new/2021/11/13/wf-lc1060.java
+++ new/2021/11/13/wf-lc1060.java
@@ -1,3 +1,5 @@
+// T: O(logN)
+// S: O(1)
 class Solution {    
     public int missingElement(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k <= 0) {
@@ -30,5 +32,31 @@ class Solution {
     
     private int getMissCount(int index, int[] nums) {
         return nums[index] - nums[0] - index;
+    }
+}
+
+
+-------------------------------------------------
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int midMissing = getMissingCounts(nums, mid);
+            if (midMissing < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return getMissingCounts(nums, left) < k ? nums[left] + (k - getMissingCounts(nums, left)) : nums[left] - (getMissingCounts(nums, left) - k + 1);
+    }
+    
+    public int getMissingCounts(int[] nums, int i) {
+        return nums[i] - nums[0] - i;
     }
 }
