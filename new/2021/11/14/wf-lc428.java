bf4a12f1c7dc982e0962a11f4653348b8f0ba2f9
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-01-28 09:14:45 -0800

    Update wf-lc428.java

--- new/2021/11/14/wf-lc428.java
+++ new/2021/11/14/wf-lc428.java
@@ -16,63 +16,74 @@ class Node {
     }
 };
 */
+
 class Codec {
     // Encodes a tree to a single string.
     public String serialize(Node root) {
         if (root == null) {
-            return "";
+            return null;
         }
-        StringBuilder sb = new StringBuilder();
         Queue<Node> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
         queue.offer(root);
+        sb.append(root.val);
+        sb.append("#");
         while (!queue.isEmpty()) {
-            Node curr = queue.poll();
-            sb.append(curr.val);
-            sb.append(",");
-            sb.append(curr.children.size());
-            for (Node node: curr.children) {
-                queue.offer(node);
+            int size = queue.size();
+            while (size-- > 0) {
+                Node curr = queue.poll();
+                List<Node> children = curr.children;
+                if (children == null || children.size() == 0) {
+                    sb.append("null");
+                } else {
+                    for (int i = 0; i < children.size(); i++) {
+                        Node child = children.get(i);
+                        sb.append(child.val);
+                        if (i != children.size() - 1) {
+                            sb.append(",");
+                        }  
+                        queue.offer(child);
+                    }
+                }
+                if (!queue.isEmpty()) {
+                    sb.append("#");
+                }  
             }
-            if (!queue.isEmpty()) {
-                sb.append(",");
-            }  
         }
-        //System.out.println(sb.toString());
-        return sb.toString();
+        String data = sb.toString();
+        return data;
     }
 	
     // Decodes your encoded data to tree.
     public Node deserialize(String data) {
-        if (data.length() == 0) {
+        if (data == null) {
             return null;
         }
-        
-        String[] nodes = data.split(",");
+        String[] elements = data.split("#");
         Queue<Node> queue = new LinkedList<>();
-        Queue<Integer> childQueue = new LinkedList<>();
-        Node root = new Node(Integer.parseInt(nodes[0]));
-        childQueue.offer(Integer.parseInt(nodes[1]));
+        Node root = new Node(Integer.parseInt(elements[0]), null);
         queue.offer(root);
-        int index = 2;
-        
-        while (!queue.isEmpty()) {
-            Node curr = queue.poll();
-            curr.children = new ArrayList<>();
-            int size = childQueue.poll();
-            for (int i = 0; i < size; i++) {
-                Node node = new Node(Integer.parseInt(nodes[index]));
-                index++;
-                childQueue.offer(Integer.parseInt(nodes[index]));
-                index++;
-                queue.offer(node);
-                curr.children.add(node);
+        for (int i = 1; i < elements.length; i++) {
+            Node parent = queue.poll();
+            List<Node> children = new ArrayList<>();
+            String[] kids = elements[i].split(",");
+            for (String kid : kids) {
+                if ("null".equals(kid)) {
+                    continue;
+                }
+                if (kid.length() == 0) {
+                    continue;
+                }
+                Node n = new Node(Integer.parseInt(kid), null);
+                queue.offer(n);
+                children.add(n);
             }
+            parent.children = children;
+            
         }
-        
         return root;
     }
 }
-
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.deserialize(codec.serialize(root));
