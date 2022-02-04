6cf5161921d46d873ecb2d17b813c8eca1a2d7cf
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-02-03 16:29:39 -0800

    Update yyf-lc1229.java

--- new/2021/12/12/yyf-lc1229.java
+++ new/2021/12/12/yyf-lc1229.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        //TODO:adding input check
         Arrays.sort(slots1, (a, b) -> a[0] - b[0]);
         Arrays.sort(slots2, (a, b) -> a[0] - b[0]);
+
         int pointer1 = 0;
         int pointer2 = 0;
+
         while (pointer1 < slots1.length && pointer2 < slots2.length) {
-            int maxStart = Math.max(slots1[pointer1][0], slots2[pointer2][0]);
-            int minEnd = Math.min(slots1[pointer1][1], slots2[pointer2][1]);
-            if (minEnd - maxStart >= duration) {
-                List<Integer> res = new ArrayList();
-                res.add(maxStart);
-                res.add(maxStart + duration);
-                return res;
+            // find the boundaries of the intersection, or the common slot
+            int intersectLeft = Math.max(slots1[pointer1][0], slots2[pointer2][0]);
+            int intersectRight = Math.min(slots1[pointer1][1], slots2[pointer2][1]);
+            if (intersectRight - intersectLeft >= duration) {
+                return new ArrayList<Integer>(Arrays.asList(intersectLeft, intersectLeft + duration));
             }
+            // always move the one that ends earlier
             if (slots1[pointer1][1] < slots2[pointer2][1]) {
                 pointer1++;
             } else {
                 pointer2++;
             }
         }
-        return new ArrayList<>();
+        return new ArrayList<Integer>();
     }
 }
