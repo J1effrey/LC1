182cc3f8ca136b5f3172093a2a2710e87304976a
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-02-11 08:58:46 -0800

    Update jian-lc25.java

--- new/2021/11/22/jian-lc25.java
+++ new/2021/11/22/jian-lc25.java
@@ -1,3 +1,5 @@
+// S: O(N)
+// T: O(N/k)
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null || head.next == null || k <= 1) {
@@ -35,5 +37,32 @@ class Solution {
         }
         
         return dummy.next;
+    }
+}
+
+-----------------------
+    
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k <= 0) {
+            return head;
+        }
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+        if (count != k) {
+            return head;
+        }
+        curr = reverseKGroup(curr, k);
+        while (count-- > 0) {
+            ListNode temp = head.next;
+            head.next = curr;
+            curr = head;
+            head = temp;
+        }
+        return curr;
     }
 }
