b78bfecb4d908aec3739268ae40f63332af8b207
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-02-23 08:59:55 -0800

    Create yyf-lc1216.java

---
+++ new/2022/02/23/yyf-lc1216.java
@@ -0,0 +1,36 @@
+// T:O(N^2)
+// S:O(N^2)
+class Solution {
+    public boolean isValidPalindrome(String str, int k) {
+        int n = str.length();
+
+        StringBuilder stringBuilder = new StringBuilder(str).reverse();
+        int lps = getLps(str, stringBuilder.toString(), n, n);
+
+        return (n - lps <= k);
+    }
+
+    /*
+    longest palindromic subsequence:
+    LCS of the given string & its reverse will be the longest palindromic sequence.
+     */
+    private int getLps(String X, String Y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+/*
+abcdeca acdca
+acedcba acdca
+*/
