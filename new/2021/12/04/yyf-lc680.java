471906d49b9358246dff75bcb21b4a2a5bfee9fa
aAbu <whujianli@gmail.com>
2022-03-08 10:28:33 -0800

    Update yyf-lc680.java

--- new/2021/12/04/yyf-lc680.java
+++ new/2021/12/04/yyf-lc680.java
@@ -1,39 +1,37 @@
-// T:O(N)
-// S:O(1)
+// O(N) N : string length
+// O(1)
 class Solution {
     public boolean validPalindrome(String s) {
         if (s == null || s.length() == 0) {
-            return false;
+            return true;
         }
-        int l = 0;
-        int r = s.length() - 1;
-        while (l < r) {
-            if (s.charAt(l) == s.charAt(r)) {
-                l++;
-                r--;
-            } else {
-                return isPalindrome(s, l, r - 1) || isPalindrome(s, l + 1, r);
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+                continue;
             }
+            
+            return isPanlindrome(s, left + 1, right) || isPanlindrome(s, left, right - 1);
         }
+        
         return true;
     }
     
-    public boolean isPalindrome(String s, int l, int r) {
-        if (l > r) {
+    private boolean isPanlindrome(String s, int left, int right) {
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+                continue;
+            }
             return false;
         }
-        while (l < r) {
-            if (s.charAt(l) != s.charAt(r)) {
-                return false;
-            }
-            l++;
-            r--;
-        }
+        
         return true;
     }
 }
-
-/*
-abca
-
-*/
