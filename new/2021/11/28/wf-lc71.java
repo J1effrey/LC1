9f48dfb1a28638da6906b90fc68ba25d6a10d7f7
aAbu <whujianli@gmail.com>
2022-03-08 14:41:38 -0800

    Update wf-lc71.java

--- new/2021/11/28/wf-lc71.java
+++ new/2021/11/28/wf-lc71.java
@@ -1,5 +1,45 @@
 // T:O(s) s is path length 
 // S:O(s)
+
+class Solution {
+    public String simplifyPath(String path) {
+        if (path == null || path.length() == 0) {
+            return "";
+        }
+        
+        String[] splited = path.split("/+");
+        LinkedList<String> paths = new LinkedList<>();
+        
+        for (int i = 0; i < splited.length; i++) {
+            String curPath = splited[i];
+            if (curPath.equals(".") || curPath.isEmpty()) {
+                continue;
+            }
+            
+            if (curPath.equals("..")) {
+                if (paths.size() > 0) {
+                    paths.removeLast();
+                }
+                continue;
+            }  
+            paths.add(curPath);
+        }
+        
+        StringBuilder sb = new StringBuilder("/");
+        
+        for (int i = 0; i < paths.size(); i++) { 
+            sb.append(paths.get(i));
+            if (i < paths.size() - 1) {
+                sb.append('/');
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+
+===========================================
+
 class Solution {
     public String simplifyPath(String path) {
         StringBuilder sb = new StringBuilder();
@@ -26,32 +66,4 @@ class Solution {
     }
 }
 
-// ==========================================================
-class Solution {
-    public String simplifyPath(String path) {
-        if (path == null || path.length() == 0) {
-            return path;
-        }
-        
-        String[] strs = path.split("/+");
-        List<String> list = new ArrayList<>();
-        
-        for (String s: strs) {
-            if (s.equals("..") ) {
-                if (list.size() > 0) {
-                    list.remove(list.size() - 1);
-                }
-            } else if (!s.equals(".") && !s.equals("")) {
-                list.add(s);
-            }
-        }
-        
-        String res = "/";
-        
-        for (String string: list) {
-            res = res + string + "/";
-        }
-        
-        return res.length() > 1 ? res.substring(0, res.length() - 1) : res;
-    }
-}
+
