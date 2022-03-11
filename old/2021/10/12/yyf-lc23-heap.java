e7d529320e330c4fedf818bb3ce1c20d143c5ac3
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-03-10 20:12:22 -0800

    Create yyf-lc23-heap.java

---
+++ old/2021/10/12/yyf-lc23-heap.java
@@ -0,0 +1,28 @@
+// O(Nlogk)
+// O(N+k)
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        ListNode dummy = new ListNode();
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode head = dummy;
+        for (ListNode list: lists) {
+            if (list == null) {
+                continue;
+            }
+            pq.offer(list);
+        }
+        
+        while (!pq.isEmpty()) {
+            ListNode curr = pq.poll();
+            head.next = curr;
+            head = head.next; // curr
+            if (head.next != null) {
+                pq.offer(head.next);
+            }
+        }
+        return dummy.next;
+    }
+}
