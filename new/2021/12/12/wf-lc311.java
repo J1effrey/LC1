e75e44078024e6b5163aca763c5d720fa7d062ac
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-03-11 08:54:36 -0800

    Update wf-lc311.java

--- new/2021/12/12/wf-lc311.java
+++ new/2021/12/12/wf-lc311.java
@@ -1,21 +1,34 @@
 //击败100%
+// T:O(rowA * colA * colB)
+// S:O(1)
 public class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
-        int m = A.length, n = A[0].length, nB = B[0].length;
-        int[][] C = new int[m][nB];
+        if (A == null || A.length == 0 || A[0] == null || A[0].length == 0) {
+            return B;
+        }
+        if (B == null || B.length == 0 || B[0] == null || B[0].length == 0) {
+            return A;
+        }
+        int rowOfA = A.length;
+        int colOfA = A[0].length;
+        int colOfB = B[0].length;
+        int[][] res = new int[rowOfA][colOfB];
 
-        for(int i = 0; i < m; i++) {
-            for(int k = 0; k < n; k++) {
-                if (A[i][k] != 0) {
-                    for (int j = 0; j < nB; j++) {
-                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+        for(int i = 0; i < rowOfA; i++) {
+            for(int j = 0; j < colOfA; j++) {
+                if (A[i][j] != 0) {
+                    for (int k = 0; k < colOfB; k++) {
+                        if (B[j][k] != 0) {
+                            res[i][k] += A[i][j] * B[j][k];
+                        }
                     }
                 }
             }
         }
-        return C;   
+        return res;   
     }
 }
+
 --------------------
 //击败10%
 class Solution {
