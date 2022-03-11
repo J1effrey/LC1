6ef6d322140fc75616a2e2af83765198410d2f7a
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-03-11 08:59:00 -0800

    Update wf-lc1277.java

--- new/2021/12/19/wf-lc1277.java
+++ new/2021/12/19/wf-lc1277.java
@@ -1,3 +1,5 @@
+// T:O(MN)
+// S:O(1)
 class Solution {
     public int countSquares(int[][] A) {
         /*
@@ -5,8 +7,8 @@ class Solution {
         dp[i][j] also means the number of squares with A[i][j] as bottom-right corner.
         */
         int res = 0;
-        for (int i = 0; i < A.length; ++i) {
-            for (int j = 0; j < A[0].length; ++j) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
                 if (A[i][j] > 0 && i > 0 && j > 0) {
                     A[i][j] = Math.min(A[i - 1][j - 1], Math.min(A[i - 1][j], A[i][j - 1])) + 1;
                 }
