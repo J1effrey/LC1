9da15764d8bec9ba1871625fd31c932c2cc9fafc
aAbu <whujianli@gmail.com>
2022-03-13 16:17:51 -0700

    Update jian-lc283.java

--- new/2021/10/27/jian-lc283.java
+++ new/2021/10/27/jian-lc283.java
@@ -1,54 +1,27 @@
+// minimum total number of operations done
+// O(n)
+// O(1)
 class Solution {
     public void moveZeroes(int[] nums) {
         if (nums == null || nums.length == 0) {
             return;
         }
-        
-        int nonZeroP = 0;
-        int zeroP = 0;
-        
-        
-        while (nonZeroP < nums.length) {
-            if (nums[nonZeroP] != 0) {
-                swap(nums, nonZeroP, zeroP);
-                zeroP++;
-            }
-            nonZeroP++;
-        }
-    }
-    
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-}
-
-
-===========
-    
-class Solution {
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
-        }
-        
         int windowSize = 0;
+        
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 windowSize++;
                 continue;
-            }
+            }    
             
-            if (windowSize > 0) {
-                swap(nums, i - windowSize, i);
-            }
+            int windowStart = i - windowSize;
+            swap(nums, windowStart, i);
         }
     }
     
-    private void swap(int[] nums, int indexA, int indexB) {
-        int temp = nums[indexA];
-        nums[indexA] = nums[indexB];
-        nums[indexB] = temp;
+    private void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
