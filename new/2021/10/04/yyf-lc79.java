74e40139c6fe09ed1f2e840907553236a7042420
aAbu <whujianli@gmail.com>
2022-03-14 22:14:02 -0700

    Update yyf-lc79.java

--- new/2021/10/04/yyf-lc79.java
+++ new/2021/10/04/yyf-lc79.java
@@ -1,66 +1,57 @@
 // Time: Time Complexity: O(Rows * Cols * 3^L) where NN is the number of cells in the board and LL is the length of the word to be matched
 // Space: O(Rows * Cols)
-class Solution {
-    private boolean[][] visited;
-    private char[][] board;
-    private int ROWS;
-    private int COLS;
+
+class Solution { 
+    int[] X_DIR = new int[] {1, -1, 0, 0};
+    int[] Y_DIR = new int[] {0, 0, 1, -1};
     
     public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0 || board[0].length == 0 || word == null) {
-            return false;
-        }
-        
-        this.board = board;
-        this.ROWS = board.length;
-        this.COLS = board[0].length;
-        this.visited = new boolean[ROWS][COLS];
-        
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                if (backTrack(i, j, word, 0)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    
-    private boolean backTrack(int row, int col, String word, int index) {
-        if (index == word.length() - 1 && word.charAt(index) == board[row][col]) {
+        if (word == null || word.length() == 0) {
             return true;
         }
         
-        if (index >= word.length()) {
+        if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
             return false;
         }
         
-        if (word.charAt(index) != board[row][col]) {
-            return false;
-        }
+        boolean[][] visited = new boolean[board.length][board[0].length];
         
-        visited[row][col] = true;
-        
-        int[] rowOffsets = new int[]{0, 1, 0, -1};
-        int[] colOffsets = new int[]{1, 0, -1, 0};
-        
-        boolean foundWord = false;
-        for (int i = 0; i < 4; i++) {
-            if (isValid(row + rowOffsets[i], col + colOffsets[i]) && !visited[row + rowOffsets[i]][col + colOffsets[i]]) {
-                foundWord = backTrack(row + rowOffsets[i], col + colOffsets[i], word, index + 1);
-                if (foundWord) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, i, j, 0, word, visited)) {
                     return true;
                 }
             }
-        }     
-        
-        visited[row][col] = false;
+        }
         
         return false;
     }
     
-    private boolean isValid(int x, int y) {
-        return x >= 0 && x < ROWS && y >= 0 && y < COLS;
+    private boolean dfs(char[][] board, int x, int y, int index, String word, boolean[][] visited) { 
+        if (index == word.length() - 1 && word.charAt(index) == board[x][y]) {
+            return true;
+        }
+    
+        
+        if (index >= word.length() || word.charAt(index) != board[x][y]) {
+            return false;
+        }
+        
+        visited[x][y] = true;
+        
+        for (int i = 0; i < 4; i++) {
+            int newX = x + X_DIR[i];
+            int newY = y + Y_DIR[i];
+            if (newX < 0 || newX >= board.length || newY < 0 || newY >= board[0].length || visited[newX][newY]) {
+                continue;
+            }
+            
+            if (dfs(board, newX, newY, index + 1, word, visited)) {
+                return true;
+            }
+        }
+        
+        visited[x][y] = false;
+        return false;
     }
 }
-
