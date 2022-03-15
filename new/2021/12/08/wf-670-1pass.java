224f06cbcee8e85b373486c1aeef90c6bbdd6a3e
aAbu <whujianli@gmail.com>
2022-03-15 11:08:08 -0700

    Update wf-670-1pass.java

--- new/2021/12/08/wf-670-1pass.java
+++ new/2021/12/08/wf-670-1pass.java
@@ -1,55 +1,34 @@
+// O(n)
+// O(1)
 class Solution {
-    /*
-    27636
-    curr: 6 (9 - 7)
-    curr: 1 (9 - 2)
-    */
     public int maximumSwap(int num) {
-        char[] nums = String.valueOf(num).toCharArray();
-        int[] numberIndex = new int[10];
+        char[] digits = Integer.toString(num).toCharArray();
         
-        for (int i = 0; i < nums.length; i++) {
-            numberIndex[nums[i] - '0'] = i;
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) { // O(N)
+            buckets[digits[i] - '0'] = i;
         }
         
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 9; j > nums[i] - '0'; j--) {
-                if (numberIndex[j] > i) {
-                    char temp = nums[i];
-                    nums[i] = nums[numberIndex[j]];
-                    nums[numberIndex[j]] = temp;
-                    return Integer.valueOf(new String(nums));
+        for (int i = 0; i < digits.length; i++) { // O(N)
+            for (int j = 9; j >= 0; j--) { // O(1)
+                if (j <= digits[i] - '0') {
+                    continue;
+                }
+                
+                int rightMostBiggerIndex = buckets[j];
+                if (rightMostBiggerIndex > i) {
+                    swap(digits, i, rightMostBiggerIndex);
+                    return Integer.valueOf(new String(digits));
                 }
             }
         }
+        
         return num;
     }
     
-    //1 pass
-    public int maximumSwap(int num) {
-        char[] chars = Integer.toString(num).toCharArray();
-        int maxIndex = chars.length - 1;
-        
-        int x = 0;
-        int y = 0;
-        
-        for (int i = chars.length - 2; i >= 0; i--) {
-            if (chars[maxIndex] == chars[i]) { 
-                continue;
-            }
-
-            if (chars[maxIndex] < chars[i]) {
-                maxIndex = i;
-            } else {
-                x = maxIndex;
-                y = i;
-            }
-        }
-
-        char temp = chars[x];
-        chars[x] = chars[y];
-        chars[y] = temp;
-
-        return Integer.valueOf(new String(chars));
+    private void swap(char[] digits, int i, int j) {
+        char temp = digits[i];
+        digits[i] = digits[j];
+        digits[j] = temp;
     }
 }
