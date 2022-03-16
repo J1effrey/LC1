d343dcc4ccf6c1b23e835aac56bd17caf1e2d80e
aAbu <whujianli@gmail.com>
2022-03-16 09:09:39 -0700

    Update wf-lc1650.java

--- new/2021/12/19/wf-lc1650.java
+++ new/2021/12/19/wf-lc1650.java
@@ -3,15 +3,20 @@
 // Space complexity: O(1)
 
 class Solution {
+    /*
+    X + Y
+    Y + X
+    */
     public Node lowestCommonAncestor(Node p, Node q) {
-        Node runner1 = p, runner2 = q;
+        Node runner1 = p;
+        Node runner2 = q;
         
         while (runner1 != runner2) { // Guaranteed to complete since both nodes belong to same tree
             // We are simulating a cycle when any of the conditions below is satisfied 
             // by pointing runner to the head of the other "list"  to make sure 
             // intersection is found before either of the below conditions are satisfied again
-            runner1 = (runner1 == null)?q:runner1.parent;
-            runner2 = (runner2 == null)?p:runner2.parent;
+            runner1 = (runner1 == null) ? q : runner1.parent;
+            runner2 = (runner2 == null) ? p : runner2.parent;
         }
         
         return runner1;
