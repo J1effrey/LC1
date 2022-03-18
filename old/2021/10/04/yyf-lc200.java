414286bc1dd3a24b88350fc7c0d566abb7a8eaba
aAbu <whujianli@gmail.com>
2022-03-17 20:10:01 -0700

    Update yyf-lc200.java

--- old/2021/10/04/yyf-lc200.java
+++ old/2021/10/04/yyf-lc200.java
@@ -1,17 +1,23 @@
-public int numIslands(char[][] grid) {
-    int count=0;
-    for(int i = 0; i < grid.length; i++)
-        for(int j = 0; j < grid[0].length; j++){
-            if(grid[i][j] == '1'){
-                dfsFill(grid, i, j);
-                count++;
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for(int i = 0; i < grid.length; i++)
+            for(int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == '1'){
+                    dfsFill(grid, i, j);
+                    res++;
+                }
             }
+        return res;
+    }
+    
+    private void dfsFill(char[][] grid,int i, int j) {
+        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] == '0') {
+            return;
         }
-    return count;
-}
-private void dfsFill(char[][] grid,int i, int j){
-    if(i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1'){
+        
         grid[i][j]='0';
+        
         dfsFill(grid, i + 1, j);
         dfsFill(grid, i - 1, j);
         dfsFill(grid, i, j + 1);
