efd8ec1af0aad3080fe486ee68056c1adc5f69e9
aAbu <whujianli@gmail.com>
2022-03-21 09:19:31 -0700

    Update yyf-lc1153.java

--- new/2021/12/19/yyf-lc1153.java
+++ new/2021/12/19/yyf-lc1153.java
@@ -1,28 +1,31 @@
+// O(N)
+// O(N)
 class Solution {
     public boolean canConvert(String str1, String str2) {
         if (str1 == null || str2 == null || str1.length() != str2.length()) {
             return false;
         }
+        
         if (str1.equals(str2)) {
             return true;
         }
-        Map<Character, Character> map = new HashMap<>();
+        
+        Map<Character, Character> coversions = new HashMap<>();
+        
         for (int i = 0; i < str1.length(); i++) {
-            if (map.containsKey(str1.charAt(i))) {
-                char ch = map.get(str1.charAt(i));
-                if (ch != str2.charAt(i)) {
-                    return false;
-                }
-            } else {
-                map.put(str1.charAt(i), str2.charAt(i));
+            char orginal = str1.charAt(i);
+            char dest = str2.charAt(i);
+            
+            if (!coversions.containsKey(orginal)) {
+                coversions.put(orginal, dest);
+            } 
+            
+            char curConversion = coversions.get(orginal);
+            if (curConversion != dest) {
+                return false;
             }
         }
         
-        return new HashSet<>(map.values()).size() < 26;
+        return new HashSet<>(coversions.values()).size() < 26;
     }
 }
-
-/*
-
-
-*/
