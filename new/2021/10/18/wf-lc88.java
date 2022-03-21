f44d2720248a7b98396c375aa1b6951c38976edd
aAbu <whujianli@gmail.com>
2022-03-21 10:55:09 -0700

    Update wf-lc88.java

--- new/2021/10/18/wf-lc88.java
+++ new/2021/10/18/wf-lc88.java
@@ -2,25 +2,33 @@
 // S: O(1)
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+        if (nums1 == null || nums1.length == 0) {
             return;
         }
-        int index = m + n - 1;
-        while (m > 0 && n > 0) {
-            if (nums1[m - 1] < nums2[n - 1]) {
-                nums1[index] = nums2[n - 1];
-                n--;
-            } else {
-                nums1[index] = nums1[m - 1];
-                m--;
-            }
-            index--;             
+        
+        if (nums2 == null || nums2.length == 0) {
+            return;
         }
         
-        while (n > 0) {
-            nums1[index] = nums2[n - 1];
-            n--;
-            index--;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int index = m + n - 1;
+        
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] >= nums2[p2]) {
+                nums1[index--] = nums1[p1--];
+                continue;
+            }
+            
+            nums1[index--] = nums2[p2--]; 
+        }
+        
+        while (p1 >= 0) {
+            nums1[index--] = nums1[p1--];
+        }
+        
+        while (p2 >= 0) {
+            nums1[index--] = nums2[p2--]; 
         }
     }
 }
