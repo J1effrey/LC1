e5ffced82d602c106bcaf283634e3d76787eb9e4
aAbu <whujianli@gmail.com>
2022-03-24 09:14:31 -0700

    Update yyf-lc846.java

--- new/2021/12/20/yyf-lc846.java
+++ new/2021/12/20/yyf-lc846.java
@@ -1,24 +1,31 @@
 // Time: O((N+MK)logM)  N是hand长度 M是不同的卡片个数 k是groupSize
 // Space: O(M)
+
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if (hand == null || hand.length == 0 || hand.length % groupSize != 0) {
             return false;
         }
+        
         Map<Integer, Integer> map = new TreeMap<>();
         for (int i : hand) { // O(N)
             map.put(i, map.getOrDefault(i, 0) + 1); // O(logM)
         }
+        
+        //TreeMap, an implementation of the SortedMap interface. its keys in sorted order.
         for (int i : map.keySet()) { // O(M)
-            if (map.get(i) > 0) { 
-                for (int j = groupSize - 1; j >= 0; j--) { // O(K)
-                    if (map.get(i) > map.getOrDefault(i + j, 0)) {
+            int curCount = map.get(i);
+            if (curCount > 0) { 
+                for (int j = 1; j < groupSize; j++) { // O(K)
+                    if (curCount > map.getOrDefault(i + j, 0)) {
                         return false;
                     }
-                    map.put(i + j, map.get(i + j) - map.get(i)); // O(logM)
+                    map.put(i + j, map.get(i + j) - curCount); // O(logM)
                 }
+                map.put(i, 0);
             }
         }
+        
         return true;
     }
 }
