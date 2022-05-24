997c61d9b8a1bb208b0574115989eaedbd8794af
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-05-24 12:03:04 -0700

    Update yyf-lc1272.java

--- new/2022/05/24/yyf-lc1272.java
+++ new/2022/05/24/yyf-lc1272.java
@@ -4,16 +4,21 @@ class Solution {
     public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
         List<List<Integer>> res = new ArrayList<>();
         for (int[] interval : intervals) {
+            // no overlap
             if (interval[1] < toBeRemoved[0] || interval[0] > toBeRemoved[1]) {
                 res.add(Arrays.asList(interval[0], interval[1]));
-            } else {
-                if (interval[0] < toBeRemoved[0]) {
-                    res.add(Arrays.asList(interval[0], toBeRemoved[0]));
-                } 
-                if (interval[1] > toBeRemoved[1]) {
-                    res.add(Arrays.asList(toBeRemoved[1], interval[1]));
-                }
+                continue;
+            } 
+            // has overlap
+            // left and extra
+            if (interval[0] < toBeRemoved[0]) {
+                res.add(Arrays.asList(interval[0], toBeRemoved[0]));
+            } 
+            // right and extra
+            if (interval[1] > toBeRemoved[1]) {
+                res.add(Arrays.asList(toBeRemoved[1], interval[1]));
             }
+            
         }
         return res;
     }
