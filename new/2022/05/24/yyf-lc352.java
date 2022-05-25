2993c558e0a4db35bbffc1d1eefc487039b95cb0
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-05-24 17:40:01 -0700

    Update yyf-lc352.java

--- new/2022/05/24/yyf-lc352.java
+++ new/2022/05/24/yyf-lc352.java
@@ -12,19 +12,28 @@ class SummaryRanges {
         }
         int[] low = set.lower(interval);
         int[] high = set.higher(interval);
+        // When we want to merge with right side, right side can't contains interval!!!
+        // why we don't need to determine low ? -----> low's start point is strictly smaller than val, because interval={val, val} 
         if (high != null && high[0] == val) {
             return;
         }
+        // can merge with left and right side
         if (low != null && low[1] + 1 == val && 
             high != null && val + 1 == high[0]) {
             low[1] = high[1];
             set.remove(high);
         }
+        // can merge with left side
+        // why >=?   ---->  when we use lower, we can only find the start that is lower than val, the end can be larger!!!
         else if (low != null && low[1] + 1 >= val) {
             low[1] = Math.max(low[1], val);
-        } else if (high != null && val + 1 == high[0]) {
+        } 
+        // can merge with right side
+        else if (high != null && val + 1 == high[0]) {
             high[0] = val;
-        } else {
+        } 
+        // cannot merge with either side
+        else {
             set.add(interval);
         }
     }
