3dfa7ab87add9c583290125d3c2b9950816f4a73
aAbu <whujianli@gmail.com>
2022-06-01 09:19:39 -0700

    Create jian-lc628.java

---
+++ new/2022/06/jian-lc628.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /*
+        all scenarios are listed as following:
+        3 positive -> 3 largest (+)
+        2 positive + 1 negative -> 1 largest (+) 2 smallest (-)
+        1 positive + 2 negative -> 1 largest (+) 2 smallest (-)
+        3 negative -> 3 largest (-)
+    */
+    public int maximumProduct(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        for (int n: nums) {
+            if (n <= min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n <= min2) {     // n lies between min1 and min2
+                min2 = n;
+            }
+            if (n >= max1) {            // n is greater than max1, max2 and max3
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n >= max2) {     // n lies betweeen max1 and max2
+                max3 = max2;
+                max2 = n;
+            } else if (n >= max3) {     // n lies betwen max2 and max3
+                max3 = n;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+}
+/*
+
+1,2,3,4, 1,2,3,4
+
+
+10**9
+*/
