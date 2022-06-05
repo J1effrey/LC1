f9b2958f55a51b84a21bad15a0e399f375879a3d
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-06-04 23:01:57 -0700

    Update jian-lc37.java

--- new/2022/01/09/jian-lc37.java
+++ new/2022/01/09/jian-lc37.java
@@ -1,6 +1,58 @@
 // T: O()
 // S: O()
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+             for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            } 
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+             }
+        }
+        return true;
+    }
+    
+    public boolean isValid(char[][] board, int i, int j, char c) {
+        // check same column
+        for (int row = 0; row < 9; row++) {
+            if (board[row][j] == c) {
+                return false;
+            }
+        }
+        
+        // check same row
+        for (int col = 0; col < 9; col++) {
+            if (board[i][col] == c) {
+                return false;
+            }
+        }
+        
+        // check 3 x 3
+        for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++) {
+            for (int col = (j / 3) * 3; col < (j / 3) * 3 + 3; col++) {
+                if (board[row][col] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
 
+-------------------------------------------------------------------------------
 class Solution {
     public void solveSudoku(char[][] board) {
         if (board == null || board.length == 0) {
