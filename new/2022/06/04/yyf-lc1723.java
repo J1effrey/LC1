05f0c49a887304c01495923582d712658f628a6b
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-06-05 00:04:34 -0700

    Create yyf-lc1723.java

---
+++ new/2022/06/04/yyf-lc1723.java
@@ -0,0 +1,26 @@
+class Solution {
+    int res = Integer.MAX_VALUE;
+    public int minimumTimeRequired(int[] jobs, int k) {
+        Arrays.sort(jobs);
+        dfs(jobs, jobs.length - 1, new int[k]);
+        return res;
+    }
+    
+    public void dfs(int[] jobs, int pos, int[] sum) {
+        if (pos < 0) {
+            res = Math.min(res, Arrays.stream(sum).max().getAsInt());
+            return;
+        }
+        if (Arrays.stream(sum).max().getAsInt() >= res) {
+            return;
+        }
+        for (int i = 0; i < sum.length; i++) {
+            if (i > 0 && sum[i] == sum[i - 1]) {
+                continue;
+            }
+            sum[i] += jobs[pos];
+            dfs(jobs, pos - 1, sum);
+            sum[i] -= jobs[pos];
+        }
+    }
+}
