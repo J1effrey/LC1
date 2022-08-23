4ac04173bba0ee2f69542bca12d8088816d68a2d
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-08-23 00:01:17 -0700

    Create yyf-lc169-d&c.java

---
+++ new/2022/08/yyf-lc169-d&c.java
@@ -0,0 +1,32 @@
+class Solution {
+    // divide and conquer
+    public int majorityElement(int[] nums) {
+        return divide(nums, 0, nums.length - 1);
+    }
+
+    public int divide(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+        int mid = left + (right - left) / 2;
+        int leftRes = divide(nums, left , mid);
+        int rightRes = divide(nums, mid + 1, right);
+        if (leftRes == rightRes) {
+            return leftRes;
+        }
+        int leftCount = conquer(nums, leftRes, left, right);
+        int rightCount = conquer(nums, rightRes, left, right);
+        return leftCount > rightCount ? leftRes : rightRes;
+    }
+    
+    // candidate must be rightRes or leftRes
+    public int conquer(int[] nums, int target, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
