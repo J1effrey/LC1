7b298275aaf6fe9e79f27a13ed78a07dc0e17eba
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-09-07 00:09:34 -0700

    Update jian-lc1438.java

--- new/2021/12/19/jian-lc1438.java
+++ new/2021/12/19/jian-lc1438.java
@@ -42,49 +42,23 @@ class Solution {
 ---------------------------------------------------------------------
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        if (nums == null || nums.length == 0) {
-            return 0;    
+        Deque<Integer> maxd = new ArrayDeque<>();
+        Deque<Integer> mind = new ArrayDeque<>();
+        int left = 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!maxd.isEmpty() && maxd.peekLast() < nums[i]) maxd.pollLast();
+            while (!mind.isEmpty() && nums[i] < mind.peekLast()) mind.pollLast();
+            maxd.add(nums[i]);
+            mind.add(nums[i]);
+            // if!!!!!!!!!!!!!
+            if (maxd.peek() - mind.peek() > limit) {
+                if (maxd.peek() == nums[left]) maxd.poll();
+                if (mind.peek() == nums[left]) mind.poll();
+                left++;
+            }
+            res = Math.max(res, i - left + 1);
         }
-        
-        // maxDeque and minDeque are used to store the max and min value in this particular subarray
-        // Array[l , r]
-        Deque<Integer> maxDeque = new LinkedList<>();
-        Deque<Integer> minDeque = new LinkedList<>();
-
-        int res = 1;
-        int l = 0;
-
-        // find the longest subarray for every right pointer by shrinking left pointer
-        for (int r = 0; r < nums.length; r++) {
-
-            // update maxDeque with new right pointer
-            while (!maxDeque.isEmpty() && maxDeque.peekLast() < nums[r]) {
-                maxDeque.removeLast();
-            }
-            maxDeque.addLast(nums[r]);
-
-            // update minDeque with new right pointer
-            while (!minDeque.isEmpty() && minDeque.peekLast() > nums[r]) {
-                minDeque.removeLast();
-            }
-            minDeque.addLast(nums[r]);
-
-            // shrink left pointer if exceed limit
-            while (maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
-                if (maxDeque.peekFirst() == nums[l]) {
-                    maxDeque.pollFirst();
-                }
-                
-                if (minDeque.peekFirst() == nums[l]) {
-                    minDeque.pollFirst();
-                }
-                l++;  // shrink it!
-            }
-
-            // update res
-            res = Math.max(res, r - l + 1);
-        }
-
         return res;
     }
 }
