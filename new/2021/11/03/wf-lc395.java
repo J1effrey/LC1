9e7f3102b4cd9ff88e8439355bc17bf3916c6915
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-09-07 00:14:31 -0700

    Update wf-lc395.java

--- new/2021/11/03/wf-lc395.java
+++ new/2021/11/03/wf-lc395.java
@@ -56,3 +56,30 @@ class Solution {
         return res;
     }
 }
+
+--------------------------------------------------------------------------------
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int res = 0;
+        for (int unique = 1; unique < 26; unique++) {
+            Map<Character, Integer> map = new HashMap<>();
+            int left = 0;
+            int validCount = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                if (map.get(c) == k) validCount++;
+                while (map.keySet().size() > unique) {
+                    char leftChar = s.charAt(left);
+                    if (map.getOrDefault(leftChar, 0) == k) validCount--;
+                    map.put(leftChar, map.getOrDefault(leftChar, 0) - 1);
+                    if (map.get(leftChar) == 0) map.remove(leftChar);
+                    left++;
+                }
+                int count = map.keySet().size();
+                if (count == unique && count == validCount) res = Math.max(i - left + 1, res);
+            }
+        }
+        return res;
+    }
+}
