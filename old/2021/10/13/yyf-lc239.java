48fab65fb2198c035c42ea9a5c441a1875da1fce
Yifei Yang <56526968+J1effrey@users.noreply.github.com>
2022-09-07 00:07:04 -0700

    Update yyf-lc239.java

--- old/2021/10/13/yyf-lc239.java
+++ old/2021/10/13/yyf-lc239.java
@@ -26,3 +26,20 @@ class Solution {
         return res;
     }
 }
+
+-----------------------------------------------------------
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int N = nums.length;
+        Deque<Integer> q = new ArrayDeque<>();
+        int[] res = new int[N - k + 1];
+        for (int i = 0; i < N; i++) {
+            int startWindowIndex = i - k + 1;
+            while (!q.isEmpty() && i - q.peekFirst() >= k) q.pollFirst(); // 左出q,保证窗口大小k - 1
+            while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) q.pollLast(); // 右出q，保证递减队列
+            q.offerLast(i);  // 进q，此时q.size == k
+            if (startWindowIndex >= 0) res[startWindowIndex] = nums[q.peekFirst()]; // 使用q左边最大值
+        }
+        return res;
+    }
+}
