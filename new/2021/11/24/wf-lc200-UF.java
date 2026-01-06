345693e9e614acf9122e9bd63762e78ba5c926bd
aAbu <whujianli@gmail.com>
2026-01-05 19:43:11 -0800

    Update wf-lc200-UF.java

--- new/2021/11/24/wf-lc200-UF.java
+++ new/2021/11/24/wf-lc200-UF.java
@@ -1,35 +1,31 @@
 class UF {
-    // 连通分量个数
     private int count;
-    // 存储每个节点的父节点
     private int[] parent;
 
-    // n 为图中节点的个数
     public UF(int n) {
         this.count = n;
         parent = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
-    }
-    
-    // 将节点 p 和节点 q 连通
+    } 
+
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
-        
-        if (rootP == rootQ)
+
+        if (rootP == rootQ) {
             return;
-        
+        }
+
         parent[rootQ] = rootP;
-        // 两个连通分量合并成一个连通分量
         count--;
     }
 
-    // 判断节点 p 和节点 q 是否连通
     public boolean connected(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
+
         return rootP == rootQ;
     }
 
@@ -37,95 +33,52 @@ class UF {
         if (parent[x] != x) {
             parent[x] = find(parent[x]);
         }
+
         return parent[x];
     }
 
-    // 返回图中的连通分量个数
-    public int count() {
+    public int getCount() {
         return count;
     }
 }
 
 
-class UnionFind {
-    int[] parents;
-    int[] size;
-    int count;
-    public UnionFind(int n) {
-        this.parents = new int[n];
-        this.size = new int[n];
-        this.count = n;
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    public int find(int x) {
-        while (x != parents[x]) {
-            parents[x] = parents[parents[x]];
-            x = parents[x];
-        }
-        return x;
-    }
-
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) {
-            return;
-        } 
-
-        if (size[rootP] > size[rootQ]) {
-            parents[rootQ] = rootP;
-            size[rootP] += size[rootQ];
-        } else {
-            parents[rootP] = rootQ;
-            size[rootQ] += size[rootP];
-        }
-        count--;
-    }
-
-    public int getCount() {
-        return this.count;
-    }
-}
-
-// O(m * n)
-// O(m * n)
 class Solution {
-    int[] X_DIR = new int[]{1, -1, 0, 0};
-    int[] Y_DIR = new int[]{0, 0, 1, -1};
-    
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }       
+
+        if (grid[0] == null || grid[0].length == 0) {
             return 0;
         }
-        
-        int row = grid.length;
-        int col = grid[0].length;
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        UF uf = new UF(m * n);
         int zeroCount = 0;
-        
-        UnionFind uf = new UnionFind(row * col);
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '0') {
                     zeroCount++;
                     continue;
                 }
-                
-                for (int k = 0; k < 4; k++) {
-                    int newX = i + X_DIR[k];
-                    int newY = j + Y_DIR[k];
-                    
-                    if (newX < 0 || newX >= row || newY < 0 || newY >= col || grid[newX][newY] == '0') {
-                        continue;
-                    }
-                    
-                    uf.union(i * col + j, newX * col + newY);
+
+                // union with its left
+                if (i > 0 && grid[i - 1][j] == '1') {
+                    uf.union(i * n + j, (i - 1) * n + j);
                 }
+
+                // union with its top
+                if (j > 0 && grid[i][j - 1] == '1') {
+                    uf.union(i * n + j, i * n + (j - 1));
+                } 
             }
         }
+
         return uf.getCount() - zeroCount;
+
     }
 }
