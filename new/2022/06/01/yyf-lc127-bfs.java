473868cb440e124e07938ac4cdcf44ddbae8cf26
aAbu <whujianli@gmail.com>
2026-01-13 09:54:20 -0800

    Update yyf-lc127-bfs.java

--- new/2022/06/01/yyf-lc127-bfs.java
+++ new/2022/06/01/yyf-lc127-bfs.java
@@ -1,36 +1,46 @@
-// ToDo: Word Ladder
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>(wordList);
-        Queue<String> q = new LinkedList<>();
+        var wordSet = new HashSet<String>(wordList);
+        if (!wordSet.contains(endWord)) {
+            return 0;
+        }
+        wordSet.remove(beginWord);
+
+        var q = new ArrayDeque<String>();
+        var visited = new HashSet<String>();
+
         q.offer(beginWord);
+        visited.add(beginWord);
         int step = 1;
-        int N = beginWord.length();
+
         while (!q.isEmpty()) {
             int size = q.size();
-            while (size-- > 0) {
-                String curr = q.poll();
-                if (curr.equals(endWord)) {
+
+            for (int i = 0; i < size; ++i) {
+                String curWord = q.poll();
+                if (curWord.equals(endWord)) {
                     return step;
                 }
-                for (int j = 0; j < N; j++) {
+                char[] chars = curWord.toCharArray();
+                for (int j = 0; j < curWord.length(); j++) {
+                    char originChar = chars[j];
                     for (char c = 'a'; c <= 'z'; c++) {
-                        StringBuilder sb = new StringBuilder(curr);
-                        sb.setCharAt(j, c);
-                        String nextWord = sb.toString();
-                        if (set.contains(nextWord)) {
-                            // early termination 
-                            if (nextWord.equals(endWord)) {
-                                return step + 1;
-                            }
-                            set.remove(nextWord);
-                            q.offer(nextWord);
+                        if (c == originChar) {
+                            continue;
+                        }
+                        chars[j] = c;
+                        String newWord = new String(chars);
+                        if (wordSet.contains(newWord) && !visited.contains(newWord)) {
+                            q.offer(newWord);
+                            visited.add(newWord);
                         }
                     }
+                    chars[j] = originChar;
                 }
             }
             step++;
         }
+
         return 0;
     }
 }
