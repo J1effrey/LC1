6c9268377b35bf109c26fa64eba799dedef1c768
aAbu <whujianli@gmail.com>
2026-02-18 11:33:06 -0800

    Update yyf-lc3.java

--- new/2022/03/10/yyf-lc3.java
+++ new/2022/03/10/yyf-lc3.java
@@ -3,39 +3,24 @@ class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
+
+        int res = 0;
         int left = 0;
         int right = 0;
-        int[] arr = new int[128];
-        Arrays.fill(arr, -1);
-        int res = 0;
-        while (right < s.length()) {
-            char ch = s.charAt(right);
-            int index = arr[ch - 0];
-            if (index != -1 && index >= left) {
-                left = index + 1;
-            }
-            arr[ch - 0] = right;
-            res = Math.max(res, right - left + 1);
-            right++;
-        }
-        return res;
-    }
-}
+        Map<Character, Integer> lastIdx = new HashMap<>();
+        int n = s.length();
 
-------------------
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int N = s.length();
-        int left = 0;
-        int res = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            char c = s.charAt(i);
-            if (map.containsKey(c)) {
-                left = Math.max(map.get(c), left);
+        while (right < n) {
+            char c = s.charAt(right);
+
+            if (lastIdx.containsKey(c) && lastIdx.get(c) >= left) {
+                left = lastIdx.get(c) + 1;
             }
-            map.put(c, i + 1);
-            res = Math.max(res, i - left + 1);
+
+            lastIdx.put(c, right);
+            right++;
+
+            res = Math.max(res, right - left);
         }
         return res;
     }
