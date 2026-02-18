0468a27a5b9a80dd795831e687991cfcb43126c6
aAbu <whujianli@gmail.com>
2026-02-18 11:19:32 -0800

    Update wf-lc75.java

--- new/2021/10/27/wf-lc75.java
+++ new/2021/10/27/wf-lc75.java
@@ -1,25 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
-        }
         int left = 0;
         int right = nums.length - 1;
         int i = 0;
+
         while (i <= right) {
             if (nums[i] == 0) {
-                swap(nums, left, i);
-                left++;
-                i++;
-            } else if (nums[i] == 2) {
-                swap(nums, right, i);
-                right--;
-            } else {
-                i++;
+                swap(nums, i++, left++);
+                continue;
+            } 
+            if (nums[i] == 2) {
+                swap(nums, i, right--);
+                continue;
             }
+
+            i++;
         }
     }
-    
+
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
