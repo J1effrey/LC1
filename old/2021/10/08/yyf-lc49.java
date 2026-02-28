bc1a435e465d2039ca214afaa5b8db87ac2c8fd3
aAbu <whujianli@gmail.com>
2026-02-28 11:12:01 -0800

    Update yyf-lc49.java

--- old/2021/10/08/yyf-lc49.java
+++ old/2021/10/08/yyf-lc49.java
@@ -1,5 +1,40 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        var anagrams = new HashMap<String, List<String>>();
+
+        for (String s : strs) {
+            String encoded = encode(s);
+            anagrams.putIfAbsent(encoded, new ArrayList<String>());
+            anagrams.get(encoded).add(s);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+
+    private String encode(String s) {
+        int[] chars = new int[26];
+
+        for (char c : s.toCharArray()) {
+            chars[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < chars.length; i++) {
+            sb.append("#");
+            sb.append(chars[i]);
+        }
+
+        return sb.toString();
+    }
+}
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         if (strs == null || strs.length == 0) {
             return res;
